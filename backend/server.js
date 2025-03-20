9a10c96fbdddeb874d4bf34fcb98869cb19c024e
Gilbert <condongilbert@gmail.com>
2025-03-20 08:30:59 -0700

    updated api for testing purposes

--- backend/server.js
+++ backend/server.js
@@ -12,3 +12,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API is working!" });
+});
